c23f6dc857fa57ceeb32df9c33829031ff552f40
깃ãhanseon97 <hanseon97@naver.com>
2024-01-02 15:31:40 +0900

    aaaaaaaaaaaaaaaa

---
+++ src/test/java/com/example/kkkk/KkkkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kkkk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkkkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
